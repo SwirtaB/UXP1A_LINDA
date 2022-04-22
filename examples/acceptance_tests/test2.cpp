cc48573b07c9d6fad41a4f762a5f849ffd8b0b5b
SwirtaB <49533902+SwirtaB@users.noreply.github.com>
2022-04-22 11:20:05 +0200

    WIP

--- examples/acceptance_tests/test2.cpp
+++ examples/acceptance_tests/test2.cpp
@@ -9,21 +9,15 @@
 
 void worker1(linda::Handle handle) {
     std::cout << "Worker1: Building tuple (\"Hello world!\")" << std::endl;
-    linda::Tuple t = linda::Tuple::Builder().String("Hello example!").build();
+    linda::Tuple t = linda::Tuple::Builder().Float(1.0).build();
     handle.out(t);
     handle.close();
 }
 
 void worker2(linda::Handle handle) {
     std::cout << "Worker2: Waiting for tuple (string:*)" << std::endl;
-    linda::TuplePattern tp = linda::TuplePattern::Builder().anyString().build();
+    linda::TuplePattern tp = linda::TuplePattern::Builder().floatOf(linda::RequirementType::Eq, 1.0).build();
     linda::Tuple        t  = handle.in(tp).value();
     std::cout << "Worker2: got: " << std::get<std::string>(t.values()[0]) << std::endl;
     handle.close();
-}
-
-int main() {
-    auto lindaServer = linda::Server({std::function(worker1), std::function(worker2)});
-    lindaServer.start();
-    return 0;
 }
